81a41f80a20d9fc0ac829f1a3903792d73e1b0aa
mimvss <stachutum@gmail.com>
2025-11-11 21:36:56 +0100

    koniec zadania, poprawka

--- myprogram.py
+++ myprogram.py
@@ -5,9 +5,9 @@ remote_work = True
 big_company = employees > 100
 income = 4500000
 income_per_person = income / employees
-blabla = False 
 
-list=[type (company),type (phone), type (employees),type (remote_work),type (big_company), type (income), type (income_per_person), type (blabla)  ]
+names=["company", "phone", "employees","remote_work","big_company", "income", "income_per_person" ]
+list=[company, phone, employees,remote_work,big_company, income, income_per_person ]
 
 str=0
 int=0
@@ -15,13 +15,14 @@ bool=0
 float=+0
 
 for i in range (len(list)):
-    print(list[i])
+    print(f'var name:{names[i]}, value: {list[i]}, value type: {type (list [i]).__name__}  ')
 
-    if list[i] == type("str"):
+
+    if type (list[i]) == type("str"):
         str = str+1
-    elif list[i] == type(2/2):
+    elif type (list[i]) == type(2/2):
         float = float+1
-    elif list[i] == type(2+2==4):
+    elif type (list[i]) == type(2+2==4):
         bool = bool+1
     else: int=int+1
 
