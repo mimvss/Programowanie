83e92508f75cbc388f005f544582e838f3be5c80
mimvss <stachutum@gmail.com>
2025-11-12 12:53:13 +0100

    poprawka

--- 5.4.py
+++ 5.4.py
@@ -2,8 +2,9 @@
 #b is an amount after vat 
 
 Amount = input('amount= ')
-a = int(Amount)
+a = float(Amount)
 v = 23 * a / 100
 vat = round(v, 2)
 b = a - v  
-print(f'Vat = {vat} (23%), your amount after vat is {b}')
+b1 = round(b, 2)
+print(f'Vat = {vat} (23%), your amount after vat is {b1}')
